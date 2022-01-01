6ed72cf2a7514add8237f068c49c4ed939bfe865
Maria Kryukova <kryukova.marie@gmail.com>
2022-01-01 12:19:42 +0300

    ls update

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const form = document. getElementById('form');
-const input = document. getElementById('input');
-const todosUL = document. getElementById('todos');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const deleteBtn = document.getElementById('deleteBtn');
+const todosUL = document.getElementById('todos');
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+deleteBtn.addEventListener('click', ()=>{
+    let todosList = document.querySelectorAll('li');
+
+    if(todosList){
+        todosList.forEach(el => {
+            el.remove();
+        })
+        updateLS();
+    }
+
+})
+
+if(todos){
+    todos.forEach( todo => addTodo(todo));
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -19,16 +37,35 @@ function addTodo(todo){
         }
         todoEl.innerHTML = todoText;
 
-        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'));
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
+            updateLS();
+        });
         
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             todoEl.remove();
+            updateLS();
         });
 
         todosUL.appendChild(todoEl);
 
         input.value = '';
+
+        updateLS();
     }
+}
+
+function updateLS(){
+    let todosList = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosList.forEach( el => todos.push({
+        text: el.innerText,
+        completed: el.classList.contains('completed')
+    }));
+
+    localStorage.setItem('todos', JSON.stringify(todos) )
 }
 
